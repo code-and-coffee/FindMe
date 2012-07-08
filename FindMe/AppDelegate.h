a0a4ded65f0e30bf1b19f9cf661fa4fa35e17eb3
Nick <npetrov.fedora@gmail.com>
2012-07-09 00:41:35 +0300

    Initial Commit

---
+++ FindMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FindMe
+//
+//  Created by Nick on 7/9/12.
+//  Copyright (c) 2012 MyCompanyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FindMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FindMe
+//
+//  Created by Nick on 7/9/12.
+//  Copyright (c) 2012 MyCompanyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
